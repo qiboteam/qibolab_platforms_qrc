11224fe388c74f1e231fe5d48cccee71ed3d17be
Alvaro Orgaz <aorgazf@gmail.com>
2023-05-30 11:48:32 +0400

    rename platform to qw5q_gold_qblox

--- qw5q_gold_qblox.py
+++ qw5q_gold_qblox.py
@@ -1,9 +1,9 @@
 import pathlib
-RUNCARD = pathlib.Path(__file__).parent / "qw5q_gold.yml"
+RUNCARD = pathlib.Path(__file__).parent / "qw5q_gold_qblox.yml"
 
 
 def create(runcard=RUNCARD):
     """QuantWare 5q-chip controlled using qblox cluster rf."""
     from qibolab.platforms.multiqubit import MultiqubitPlatform
 
-    return MultiqubitPlatform("qw5q_gold", runcard)
+    return MultiqubitPlatform("qw5q_gold_qblox", runcard)
