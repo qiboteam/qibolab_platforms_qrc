9d3ab5afbfb5ebf5a778e857d96a281e9943c3b6
rodolfocarobene <rodolfo.carobene@gmail.com>
2023-07-31 16:37:53 +0400

    line D qw25

--- tii_zcu216.py
+++ tii_zcu216.py
@@ -11,6 +11,7 @@ ADDRESS = "192.168.0.85"
 PORT = 6000
 RUNCARD = pathlib.Path(__file__).parent / "tii_zcu216.yml"
 
+TWPA_ADDRESS = "192.168.0.31"
 LO_ADDRESS = "192.168.0.35"
 
 
@@ -22,32 +23,56 @@ def create(runcard=RUNCARD):
     # Instantiate QICK instruments
     controller = RFSoC(NAME, ADDRESS, PORT)
     controller.cfg.adc_trig_offset = 190
-    controller.cfg.repetition_duration = 200
+    controller.cfg.repetition_duration = 50
     # Create channel objects
     channels = ChannelMap()
 
-    channels |= Channel("L3-18", port=controller[7])  # readout
+    channels |= Channel("L3-30", port=controller[6])  # readout
 
-    # qubit C4
-    channels |= Channel("L2-04-0", port=controller[0])  # feedback
-    channels |= Channel("L1-19", port=controller[1])  # flux
-    channels |= Channel("L4-26", port=controller[5])  # drive
+    # qubit D1
+    channels |= Channel("L2-01-0", port=controller[0])  # feedback
+    channels |= Channel("L1-21", port=controller[9])  # flux
+    channels |= Channel("L4-28", port=controller[0])  # drive
+
+    # qubit D2
+    channels |= Channel("L2-01-1", port=controller[1])  # feedback
+    channels |= Channel("L1-22", port=controller[7])  # flux
+    channels |= Channel("L4-29", port=controller[4])  # drive
+
+    # qubit D3
+    channels |= Channel("L2-01-2", port=controller[2])  # feedback
+    channels |= Channel("L1-23", port=controller[8])  # flux
+    channels |= Channel("L4-30", port=controller[2])  # drive
+
+    twpa_lo = SGS100A("TWPA", TWPA_ADDRESS)
+    twpa_lo.frequency = 5_300_000_000
+    twpa_lo.power = -5
 
     readout_lo = SGS100A("LO", LO_ADDRESS)
-    readout_lo.frequency = 7.5e9
+    readout_lo.frequency = 7.6e9
     readout_lo.power = 10
-    channels["L3-18"].local_oscillator = readout_lo
+    channels["L3-30"].local_oscillator = readout_lo
 
-    local_oscillators = [readout_lo]
+    local_oscillators = [readout_lo, twpa_lo]
     instruments = [controller] + local_oscillators
 
     platform = Platform(NAME, runcard, instruments, channels)
 
     # assign channels to qubits
     qubits = platform.qubits
-    qubits[0].readout = channels["L3-18"]
-    qubits[0].feedback = channels["L2-04-0"]
-    qubits[0].drive = channels["L4-26"]
-    qubits[0].flux = channels["L1-19"]
+    qubits["D1"].readout = channels["L3-30"]
+    qubits["D1"].feedback = channels["L2-01-0"]
+    qubits["D1"].drive = channels["L4-28"]
+    # qubits['D1'].flux     = channels["L1-21"]
+
+    qubits["D2"].readout = channels["L3-30"]
+    qubits["D2"].feedback = channels["L2-01-1"]
+    qubits["D2"].drive = channels["L4-29"]
+    # qubits['D2'].flux     = channels["L1-22"]
+
+    qubits["D3"].readout = channels["L3-30"]
+    qubits["D3"].feedback = channels["L2-01-2"]
+    qubits["D3"].drive = channels["L4-30"]
+    # qubits['D3'].flux     = channels["L1-23"]
 
     return platform
