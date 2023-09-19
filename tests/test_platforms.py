7e47e7c6ade1848fc11c317d098a20aef0ecf596
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2023-09-19 12:22:56 +0400

    Review comments

--- tests/test_platforms.py
+++ tests/test_platforms.py
@@ -4,21 +4,13 @@ import pathlib
 import pytest
 from qibolab import Platform, create_platform
 
-PATH = pathlib.Path(__file__).parent.parent
+PATH = pathlib.Path(__file__).parents[1]
 
 
-def platform_names():
-    """Yield the names of all platforms in the repository."""
-    for filename in os.listdir(PATH):
-        split = filename.split(".")
-        if split[-1] == "py":
-            yield split[0]
-
-
-@pytest.mark.parametrize("name", platform_names())
-def test_create(name):
-    """Test that platform ``name`` can be created."""
-    platform = create_platform(name)
+@pytest.mark.parametrize("path", PATH.glob("*.py"))
+def test_create(path):
+    """Test that platform can be created."""
+    platform = create_platform(path.stem)
     qubit = next(iter(platform.qubits))
     rx_pulse = platform.create_RX_pulse(qubit)
     mz_pulse = platform.create_MZ_pulse(qubit, start=rx_pulse.duration)
