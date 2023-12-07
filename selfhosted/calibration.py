6abf02bf9fd4a2b04eabb8893b29c1816eb44af3
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2023-12-07 19:39:57 +0400

    Selfhosted continues even if a routine fails

--- selfhosted/calibration.py
+++ selfhosted/calibration.py
@@ -1,6 +1,11 @@
 import argparse
+import logging
 import pathlib
+import traceback
+from dataclasses import dataclass, field
+from typing import Callable, List, Optional
 
+from qibocal.auto.operation import Routine
 from qibocal.protocols.characterization import Operation
 from qibolab import create_platform
 
@@ -10,68 +15,96 @@ parser = argparse.ArgumentParser()
 parser.add_argument("name", type=str, help="Name of the platform.")
 
 
-def generate_message(name, assigment_fidelities, qnds, t1s, t2s, time):
-    # def generate_message(name, assigment_fidelities, qnds, t1s, t2s, gate_fidelity, time):
-    """Generates message that is added to GitHub comments."""
-    path = pathlib.Path.cwd() / MESSAGE_FILE
-    with open(path, "w") as file:
-        file.write(f"Run on platform `%s` completed in %.2fsec! :atom:" % (name, time))
-        file.write("\n\nT1:\n")
-        for qubit, t1 in t1s.items():
-            file.write(f"{qubit}: {t1/1000:.2f} us\n")
-        file.write("\n\nT2:\n")
-        for qubit, t2 in t2s.items():
-            file.write(f"{qubit}: {t2/1000:.2f} us\n")
-        file.write("\n\nReadout assignment fidelities:\n")
-        for qubit, fidelity in assigment_fidelities.items():
-            file.write(f"{qubit}: {fidelity:.3f}\n")
-        file.write("\n\nReadout QND:\n")
-        # for qubit, qund in qnds.items():
-        #     file.write(f"{qubit}: {qnd:.3f}\n")
-        # # FIXME: several gate fidelities when RB data gets changed
-        # file.write("\n\nGate fidelities:\n")
-        # file.write(f" {gate_fidelity:.3f}\n")
+@dataclass
+class Result:
+    header: str
+    attribute: str
+    formatter: Callable = field(default=lambda x: f"{x:.3f}")
+
+
+@dataclass
+class Experiment:
+    routine: Routine
+    params: dict
+    results: List[Result]
+    fit: Optional["test"] = None
+    acquisition_time: float = 0
+    fit_time: float = 0
+
+    @property
+    def total_time(self):
+        return self.acquisition_time + self.fit_time
+
+    def __call__(self, platform, qubits):
+        params = self.routine.parameters_type.load(self.params)
+        try:
+            data, self.acquisition_time = self.routine.acquisition(
+                params=params, platform=platform, qubits=qubits
+            )
+            self.fit, self.fit_time = self.routine.fit(data)
+        except:
+            logging.error(traceback.format_exc())
+
+    def report(self, file):
+        for result in self.results:
+            file.write(f"\n{result.header}:")
+            if self.fit is None:
+                file.write(" execution failed :worried:")
+            else:
+                file.write("\n")
+                for qubit, value in getattr(self.fit, result.attribute).items():
+                    file.write(f"{qubit}: {result.formatter(value)}\n")
+
+
+def convert_to_us(x):
+    return f"{x / 1000:.2f} us"
+
+
+EXPERIMENTS = [
+    Experiment(
+        Operation.readout_characterization.value,
+        dict(nshots=10000),
+        [
+            Result("Readout assignment fidelities", "assignment_fidelity"),
+            Result("Readout QND", "qnd"),
+        ],
+    ),
+    Experiment(
+        Operation.t1_signal.value,
+        dict(
+            delay_before_readout_start=50,
+            delay_before_readout_end=200000,
+            delay_before_readout_step=2500,
+            nshots=1024,
+        ),
+        [Result("T1", "t1", convert_to_us)],
+    ),
+    Experiment(
+        Operation.t2_signal.value,
+        dict(
+            delay_between_pulses_start=50,
+            delay_between_pulses_end=200000,
+            delay_between_pulses_step=2500,
+            nshots=1024,
+        ),
+        [
+            Result("T2", "t2", convert_to_us),
+        ],
+    )
+    # Experiment(
+    #     "Gate fidelities",
+    #     Operation.standard_rb.value,
+    #     dict(
+    #         depths=[10, 100, 150, 200, 250, 300],
+    #         niter=8,
+    #         nshots=256,
+    #     )
+    # )
+]
 
 
 def main(name):
     """Execute single shot classification routine on the given platform."""
-    routines = [
-        Operation.readout_characterization.value,
-        Operation.t1_msr.value,
-        Operation.t2_msr.value,
-        # Operation.standard_rb.value,
-    ]
-
-    parameters = [
-        routines[0].parameters_type.load(dict(nshots=10000)),
-        routines[1].parameters_type.load(
-            dict(
-                delay_before_readout_start=50,
-                delay_before_readout_end=200000,
-                delay_before_readout_step=2500,
-                nshots=1024,
-            )
-        ),
-        routines[2].parameters_type.load(
-            dict(
-                delay_between_pulses_start=50,
-                delay_between_pulses_end=200000,
-                delay_between_pulses_step=2500,
-                nshots=1024,
-            )
-        ),
-        # routines[3].parameters_type.load(
-        #     dict(
-        #         depths=[10, 100, 150, 200, 250, 300],
-        #         niter=8,
-        #         nshots=256,
-        #     )
-        # ),
-    ]
-
-    data = []
-    acquisition_time = 0
-
     platform = create_platform(name)
     qubits = platform.qubits
 
@@ -79,30 +112,20 @@ def main(name):
     platform.setup()
     platform.start()
 
-    for routine, params in zip(routines, parameters):
-        d, time = routine.acquisition(params=params, platform=platform, qubits=qubits)
-        data.append(d)
-        acquisition_time += time
+    for experiment in EXPERIMENTS:
+        experiment(platform, qubits)
 
     platform.stop()
     platform.disconnect()
 
-    fits = []
-    fit_time = 0
-    for routine, d in zip(routines, data):
-        fit, time = routine.fit(d)
-        fits.append(fit)
-        fit_time += time
-
-    generate_message(
-        name,
-        fits[0].fidelity,
-        fits[0].qnd,
-        fits[1].t1,
-        fits[2].t2,
-        # fits[3].pulse_fidelity,
-        acquisition_time + fit_time,
-    )
+    total_time = sum(experiment.total_time for experiment in EXPERIMENTS)
+    path = pathlib.Path.cwd() / MESSAGE_FILE
+    with open(path, "w") as file:
+        file.write(
+            f"Run on platform `%s` completed in %.2fsec! :atom:\n" % (name, total_time)
+        )
+        for experiment in EXPERIMENTS:
+            experiment.report(file)
 
 
 if __name__ == "__main__":
