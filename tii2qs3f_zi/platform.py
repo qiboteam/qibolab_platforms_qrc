b4515c9d4268bcf17bfd992e243c7c69aefe5ae0
Hayk Sargsyan <52532457+hay-k@users.noreply.github.com>
2024-06-21 13:14:51 +0400

    adapt to recent changes introducing concept of components

--- tii2qs3f_zi/platform.py
+++ tii2qs3f_zi/platform.py
@@ -4,8 +4,8 @@ from laboneq.dsl.device import create_connection
 from laboneq.dsl.device.instruments import HDAWG, PQSC, SHFQC
 from laboneq.simple import DeviceSetup
 from qibolab import Platform
+from qibolab.components import AcquireChannel, DcChannel, IqChannel, OscillatorConfig
 from qibolab.instruments.zhinst import (
-    ZiAcquisitionChannel,
     ZiAcquisitionConfig,
     ZiChannel,
     ZiDcConfig,
@@ -13,7 +13,7 @@ from qibolab.instruments.zhinst import (
     Zurich,
 )
 from qibolab.serialize import (
-    load_channel_configs,
+    load_component_config,
     load_instrument_settings,
     load_qubits,
     load_runcard,
@@ -46,48 +46,54 @@ def create():
 
     runcard = load_runcard(FOLDER)
     qubits, _, pairs = load_qubits(runcard)
-    channel_configs = load_channel_configs(runcard)
     settings = load_settings(runcard)
 
-    channels = []
+    components = {}
+    drive_lo = "qubit_1_2/drive/lo"
+    measure_lo = "qubit_1_2/measure/lo"
+    components[drive_lo] = load_component_config(runcard, drive_lo, OscillatorConfig)
+    components[measure_lo] = load_component_config(runcard, measure_lo, OscillatorConfig)
+    zi_channels = []
     for q in range(N_QUBITS):
-        qubits[q].acquisition = ZiAcquisitionChannel(
-            f"qubit_{q}/acquire",
-            ZiAcquisitionConfig(**channel_configs[f"qubit_{q}/acquire"]),
-            "device_shfqc",
-            "QACHANNELS/0/INPUT",
-            None,
+        measure_name = f"qubit_{q}/measure"
+        acquisition_name = f"qubit_{q}/acquire"
+        components[acquisition_name] = load_component_config(runcard, acquisition_name, ZiAcquisitionConfig)
+        qubits[q].acquisition = AcquireChannel(
+            name=acquisition_name,
+            twpa_pump=None,
+            measure=measure_name,
         )
-        channels.append(qubits[q].acquisition)
+        zi_channels.append(ZiChannel(qubits[q].acquisition, device="device_shfqc", path="QACHANNELS/0/INPUT"))
 
-        qubits[q].readout = ZiChannel(
-            f"qubit_{q}/measure",
-            ZiIqConfig(**channel_configs[f"qubit_{q}/measure"]),
-            "device_shfqc",
-            "QACHANNELS/0/OUTPUT",
+        components[measure_name] = load_component_config(runcard, measure_name, ZiIqConfig)
+        qubits[q].measure = IqChannel(
+            name=measure_name,
+            lo=measure_lo,
+            mixer=None,
+            acquisition=acquisition_name
         )
-        channels.append(qubits[q].readout)
+        zi_channels.append(ZiChannel(qubits[q].measure, device="device_shfqc", path="QACHANNELS/0/OUTPUT"))
 
-        qubits[q].drive = ZiChannel(
-            f"qubit_{q}/drive",
-            ZiIqConfig(**channel_configs[f"qubit_{q}/drive"]),
-            "device_shfqc",
-            f"SGCHANNELS/{q}/OUTPUT",
+        drive_name = f"qubit_{q}/drive"
+        components[drive_name] = load_component_config(runcard, drive_name, ZiIqConfig)
+        qubits[q].drive = IqChannel(
+            name=drive_name,
+            mixer=None,
+            lo=drive_lo,
         )
-        channels.append(qubits[q].drive)
+        zi_channels.append(ZiChannel(qubits[q].drive, device="device_shfqc", path=f"SGCHANNELS/{q}/OUTPUT"))
 
-    qubits[1].flux = ZiChannel(
-        "qubit_1/flux",
-        ZiDcConfig(**channel_configs["qubit_1/flux"]),
-        "device_hdawg",
-        "SIGOUTS/0",
+    flux_name = "qubit_1/flux"
+    components[flux_name] = load_component_config(runcard, flux_name, ZiDcConfig)
+    qubits[1].flux = DcChannel(
+        name=flux_name,
     )
-    channels.append(qubits[1].flux)
+    zi_channels.append(ZiChannel(qubits[1].flux, device="device_hdawg", path="SIGOUTS/0"))
 
     controller = Zurich(
         "ZURO_BLANCO",
         device_setup=device_setup,
-        channels=channels,
+        channels=zi_channels,
         time_of_flight=75,
         smearing=50,
     )
@@ -98,6 +104,7 @@ def create():
         str(FOLDER),
         qubits,
         pairs,
+        components,
         instruments,
         settings,
         resonator_type="3D",
