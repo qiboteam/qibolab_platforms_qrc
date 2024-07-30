2a18ff7b2a2545360ca35d399463fb215804ca34
jevillegasd <jevillegasd@gmail.com>
2024-07-30 16:02:57 +0400

    work update 30/07/24

--- tii_3qcrc1a/platform.py
+++ tii_3qcrc1a/platform.py
@@ -13,55 +13,77 @@ from qibolab.serialize import (
     load_runcard,
     load_settings,
 )
-
-NAME = "tii_3qcrc1a"
-ADDRESS = "192.168.0.20"
-FOLDER = pathlib.Path(__file__).parent
-
-
+ 
+PLATFORM = "tii_3qcrc1a"
+ADDRESS = "192.168.0.2"
+FOLDER  = pathlib.Path(__file__).parent
+ 
+ 
 def create():
-    """"QRC QFoundry 3Q Chip for Cross Resonance Gates"""
+    """"QRC QFoundry 2Q Chip for Cross Resonance Gates"""
     runcard = load_runcard(FOLDER)
 
     modules = {
-        "qrm_rf0": QrmRf("qrm_rf0", f"{ADDRESS}:4"),  # feedline
-        "qcm_rf0": QcmRf("qcm_rf1", f"{ADDRESS}:20"),  # q1, q2
+        "qrm_rf0": QrmRf("qrm_rf0", f"{ADDRESS}:1"),  # feedline
+        "qcm_rf0": QcmRf("qcm_rf0", f"{ADDRESS}:10"),  # q1, q2,  for now
+        "qrm_rf1": QrmRf("qrm_rf1", f"{ADDRESS}:2"),  # q3
+        "qrm_rf2": QrmRf("qrm_rf2", f"{ADDRESS}:3"),  # qw
     }
 
+
     controller = QbloxController("qblox_controller", ADDRESS, modules)
+    #twpa_pump0 = SGS100A(name="twpa_pump0", address="192.168.0.37")
+ 
+    controller.modules["qrm_rf0"].device
+
+
+
     instruments = {
         controller.name: controller,
     }
     instruments.update(modules)
-    channels = ChannelMap()
-
-    # Readout
-    channels |= Channel(name="W5", port=modules["qrm_rf0"].ports("o1"))
-
-    # Feedback
-    channels |= Channel(name="V2", port=modules["qrm_rf0"].ports("i1", out=False))
-
-     # Drive
-    channels |= Channel(name="V6", port=modules["qcm_rf0"].ports("o1"))
-    channels |= Channel(name="V7", port=modules["qcm_rf0"].ports("o2"))
-
-    # create qubit objects
-    qubits, _, pairs = load_qubits(runcard)
-
-    # assign readout channels to qubits
-    for q in range(0, 2):  # q0, q1
-        qubits[q].readout = channels["V2"]
-        qubits[q].feedback = channels["W5"]
-
-    # assign drive channels to qubits
-    qubits[0].drive = channels[f"V6"]
-    qubits[1].drive = channels[f"V7"]
-    channels[f"V6"].qubit = qubits[0]
-    channels[f"V7"].qubit = qubits[1]
-
-    settings = load_settings(runcard)
     instruments = load_instrument_settings(runcard, instruments)
+ 
+    # #########################################################################################################
+ 
+    # Create channel objects
+    channels = ChannelMap()
+    # Readout
+    channels |= Channel(name="V1", port=modules["qrm_rf0"].ports("o1"))
+    # Feedback
+    channels |= Channel(name="W3", port=modules["qrm_rf0"].ports("i1",out=False))
+     # Drive
+    channels |= Channel(name="W1", port=modules["qcm_rf0"].ports("o1"))
+    channels |= Channel(name="W2", port=modules["qcm_rf0"].ports("o2"))
+
+    channels |= Channel(name="W4", port=modules["qrm_rf1"].ports("o1"))
+    channels |= Channel(name="V4", port=modules["qrm_rf2"].ports("o1"))
+ 
+    channels |= Channel(name="dummy1", port=modules["qrm_rf1"].ports("i1",out=False))
+    channels |= Channel(name="dummy2", port=modules["qrm_rf2"].ports("i1",out=False))
+    
+    # create qubit objects
+    qubits, couplers, pairs = load_qubits(runcard)
+ 
+    # assign channels to qubits    
+    for q in range(0, 4):  # q0, q1
+        qubits[q].readout = channels["V1"]
+        qubits[q].feedback = channels["W3"]
+    
+    qubits[0].drive = channels[f"W1"]
+    qubits[1].drive = channels[f"W2"]
+    qubits[2].drive = channels[f"W4"]
+    qubits[3].drive = channels[f"V4"]
+
+    channels[f"W1"].qubit = qubits[0]
+    channels[f"W2"].qubit = qubits[1]
+    channels[f"W4"].qubit = qubits[2]
+    channels[f"V4"].qubit = qubits[3]
+ 
+    settings = load_settings(runcard)
+ 
+    #instruments["qblox_controller"].device
 
     return Platform(
-        str(FOLDER), qubits, pairs, instruments, settings, resonator_type="2D"
+        PLATFORM, qubits, pairs, instruments, settings, resonator_type="2D"
     )
