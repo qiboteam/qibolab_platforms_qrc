98d7158238384d19f16655e3d038166421c0d5a2
Alessandro Candido <candido.ale@gmail.com>
2025-01-29 21:39:22 +0100

    fix: Infer los for output channels

--- iqm5q/platform.py
+++ iqm5q/platform.py
@@ -2,11 +2,11 @@ import pathlib
 
 from qibolab import Platform, Qubit
 from qibolab._core.instruments.qblox.cluster import Cluster
-from qibolab._core.instruments.qblox.platform import map_ports
+from qibolab._core.instruments.qblox.platform import infer_los, map_ports
 from qibolab._core.platform.platform import QubitMap
 
-# from qibolab.instruments.qblox import Cluster, QbloxConfigs, map_ports
-from qibolab.instruments.rohde_schwarz import SGS100A
+# from qibolab.instruments.qblox import Cluster, map_ports
+# from qibolab.instruments.rohde_schwarz import SGS100A
 
 FOLDER = pathlib.Path(__file__).parent
 NAME = "iqm5q_qblox"
@@ -35,12 +35,21 @@ def create():
 
     # Create channels and connect to instrument ports
     channels = map_ports(CLUSTER, qubits, couplers)
+    los = infer_los(CLUSTER)
 
     # update channel information beyond connections
-    for q in qubits.values():
+    for i, q in qubits.items():
         if q.acquisition is not None:
             channels[q.acquisition] = channels[q.acquisition].model_copy(
                 update={"twpa_pump": "twpa"}
+            )
+        if q.probe is not None:
+            channels[q.probe] = channels[q.probe].model_copy(
+                update={"lo": los[i, True]}
+            )
+        if q.drive is not None:
+            channels[q.drive] = channels[q.drive].model_copy(
+                update={"lo": los[i, False]}
             )
 
     controller = Cluster(name=NAME, address=ADDRESS, channels=channels)
