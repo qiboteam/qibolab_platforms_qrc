fe3e67c8b1c790f29ff4ae2be2d10417e29fb224
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2025-02-20 15:58:41 +0400

    chore: update calibration and enable twpa

--- iqm5q/platform.py
+++ iqm5q/platform.py
@@ -4,9 +4,7 @@ from qibolab import Platform, Qubit
 from qibolab._core.instruments.qblox.cluster import Cluster
 from qibolab._core.instruments.qblox.platform import infer_los, map_ports
 from qibolab._core.platform.platform import QubitMap
-
-# from qibolab.instruments.qblox import Cluster, map_ports
-# from qibolab.instruments.rohde_schwarz import SGS100A
+from qibolab.instruments.rohde_schwarz import SGS100A
 
 FOLDER = pathlib.Path(__file__).parent
 NAME = "iqm5q_qblox"
@@ -31,7 +29,9 @@ CLUSTER = {
 def create():
     """IQM 5q-chip controlled with a Qblox cluster."""
     qubits: QubitMap = {i: Qubit.default(i) for i in range(5)}
-    couplers: QubitMap = {f"c{i}": Qubit.default(f"c{i}") for i in (0, 1, 3, 4)}
+    couplers: QubitMap = {
+        f"coupler_{i}": Qubit.default(f"coupler_{i}") for i in (0, 1, 3, 4)
+    }
 
     # Create channels and connect to instrument ports
     channels = map_ports(CLUSTER, qubits, couplers)
@@ -53,8 +53,7 @@ def create():
             )
 
     controller = Cluster(name=NAME, address=ADDRESS, channels=channels)
-    # instruments = {"qblox": controller, "twpa": SGS100A(address="192.168.0.35")}
-    instruments = {"qblox": controller}
+    instruments = {"qblox": controller, "twpa": SGS100A(address="192.168.0.35")}
     return Platform.load(
         path=FOLDER, instruments=instruments, qubits=qubits, couplers=couplers
     )
