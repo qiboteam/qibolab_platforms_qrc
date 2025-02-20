da3df1568b6393770513c8dda811391a4b53b37c
Alessandro Candido <candido.ale@gmail.com>
2025-02-20 15:19:09 +0100

    fix: Introduce los in test platform
    For real...

--- qblox-test/platform.py
+++ qblox-test/platform.py
@@ -2,7 +2,7 @@
 
 from qibolab import Hardware, Qubit
 from qibolab._core.instruments.qblox.cluster import Cluster
-from qibolab._core.instruments.qblox.platform import map_ports
+from qibolab._core.instruments.qblox.platform import infer_los, map_ports
 
 NAME = "qblox-test"
 ADDRESS = "192.168.0.21"
@@ -24,6 +24,23 @@ def create():
 
     # Create channels and connect to instrument ports
     channels = map_ports(CLUSTER, qubits)
+    los = infer_los(CLUSTER)
+
+    # update channel information beyond connections
+    for i, q in qubits.items():
+        if q.acquisition is not None:
+            channels[q.acquisition] = channels[q.acquisition].model_copy(
+                update={"twpa_pump": "twpa"}
+            )
+        if q.probe is not None:
+            channels[q.probe] = channels[q.probe].model_copy(
+                update={"lo": los[i, True]}
+            )
+        if q.drive is not None:
+            channels[q.drive] = channels[q.drive].model_copy(
+                update={"lo": los[i, False]}
+            )
+
     controller = Cluster(name=NAME, address=ADDRESS, channels=channels)
 
     return Hardware(instruments={"qblox": controller}, qubits=qubits)
