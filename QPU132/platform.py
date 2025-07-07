718d634bdd26c32b8109a45f5259a5340ad00ea4
Laura Martinez <laura.lmg1995@gmail.com>
2025-07-07 13:31:19 +0000

    platforms for QPU130, QPU132

---
+++ QPU132/platform.py
@@ -0,0 +1,85 @@
+import pathlib
+
+from qibolab.channels import Channel, ChannelMap
+from qibolab.instruments.qblox.cluster_qcm_bb import QcmBb
+from qibolab.instruments.qblox.cluster_qcm_rf import QcmRf
+from qibolab.instruments.qblox.cluster_qrm_rf import QrmRf
+from qibolab.instruments.qblox.controller import QbloxController
+from qibolab.instruments.rohde_schwarz import SGS100A
+from qibolab.platform import Platform
+from qibolab.serialize import (
+    load_instrument_settings,
+    load_qubits,
+    load_runcard,
+    load_settings,
+)
+
+ADDRESS = "192.168.0.2"
+FOLDER = pathlib.Path(__file__).parent
+PLATFORM = FOLDER.name
+NUM_QUBITS = 4
+
+
+def create():
+    """QRC QFoundry 4Q Chip (QPU-132) for Cross Resonance Gates"""
+    runcard = load_runcard(FOLDER)
+
+    # Declare RF Instrument
+    modules = {
+        "qrm_rf10": QrmRf("qrm_rf10", f"{ADDRESS}:10"),  # RO: q0, q1, q2, q3
+        "qcm_rf5": QcmRf("qcm_rf5", f"{ADDRESS}:5"),  # Drive: q0, q1
+        "qcm_rf6": QcmRf("qcm_rf6", f"{ADDRESS}:6"),  # Drive: q2, q3
+    }
+    controller = QbloxController("qblox_controller", ADDRESS, modules)
+
+    # Declare TWPA Instrument
+    # twpa = SGS100A(name="twpa", address="192.168.0.36")
+
+    #  Define the instruments and update all modules with the platform settings
+    instruments = {
+        controller.name: controller,
+        # twpa.name: twpa
+    }
+    instruments.update(modules)
+    instruments = load_instrument_settings(runcard, instruments)
+
+    # #########################################################################################################
+
+    # Create channel objects
+    channels = ChannelMap()
+    # Readout
+    channels |= Channel(name="feed_in", port=modules["qrm_rf10"].ports("o1"))
+    # Feedback
+    channels |= Channel(
+        name="feed_back", port=modules["qrm_rf10"].ports("i1", out=False)
+    )
+
+    # Drive
+    channels |= Channel(name="drive0", port=modules["qcm_rf5"].ports("o1"))
+    channels |= Channel(name="drive2", port=modules["qcm_rf6"].ports("o1"))
+    channels |= Channel(name="drive1", port=modules["qcm_rf5"].ports("o2"))
+    channels |= Channel(name="drive3", port=modules["qcm_rf6"].ports("o2"))
+
+    # channels |= Channel(name="dummy", port=modules["qcm_rf1"].ports("o2"))
+
+    # Channel for TWPA Pump
+
+    # channels |= Channel(name="twpa", port=None)
+    # channels["twpa"].local_oscillator = twpa
+
+    # create qubit objects
+    qubits, couplers, pairs = load_qubits(runcard)
+
+    # assign channels to qubits
+    for q, qubit in qubits.items():
+        qubit.readout = channels["feed_in"]
+        qubit.feedback = channels["feed_back"]
+        qubit.drive = channels[f"drive{q}"]
+        # qubit.twpa = channels["twpa"]
+        channels[f"drive{q}"].qubit = qubit
+
+    settings = load_settings(runcard)
+    print(runcard)
+    # instruments["qblox_controller"].device
+
+    return Platform(PLATFORM, qubits, pairs, instruments, settings, resonator_type="D")
