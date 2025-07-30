cb3f5b19b9b4fa2465c4d8fcf0c36d6a0c3b6fc9
jevillegasd <jevillegasd@gmail.com>
2025-07-30 09:26:13 +0000

    qpu130

--- qpu130/platform.py
+++ qpu130/platform.py
@@ -1,27 +1,10 @@
 import pathlib
-
-from qibolab import (
-    AcquisitionChannel,
-    Channel,
-    ConfigKinds,
-    DcChannel,
-    IqChannel,
-    Platform,
-    Qubit,
-)
-
-from qibolab.instruments.qblox.cluster_qcm_bb import QcmBb
-from qibolab.instruments.qblox.cluster_qcm_rf import QcmRf
-from qibolab.instruments.qblox.cluster_qrm_rf import QrmRf
-from qibolab.instruments.qblox.controller import QbloxController
+ 
+from qibolab import Platform, Qubit
+from qibolab._core.instruments.qblox.cluster import Cluster
+from qibolab._core.instruments.qblox.platform import infer_los, map_ports
+from qibolab._core.platform.platform import QubitMap
 from qibolab.instruments.rohde_schwarz import SGS100A
-from qibolab.platform import Platform
-from qibolab.serialize import (
-    load_instrument_settings,
-    load_qubits,
-    load_runcard,
-    load_settings,
-)
 
 ADDRESS = "192.168.0.2"
 FOLDER  = pathlib.Path(__file__).parent
@@ -30,73 +13,43 @@ NUM_QUBITS = 4
 
 ROOT = pathlib.Path.home()
 
+# the only other cluster of the config
+CLUSTER = {
+    "qrm_rf0": (18, {"io1": [0, 1, 2,3]}),
+    #"qrm_rf1": (20, {"io1": [2, 3, 4]}),
+    "qcm_rf0": (4, {1: [0], 2: [1]}),
+    "qcm_rf1": (6, {1: [2], 2: [3]}),
+    #"qcm_rf2": (8, {1: [0]}),
+    #"qcm_rf3": (8, {1: [0]}),
+    
+}
+"""Connections compact representation."""
+
 def create():
-    """"QRC QFoundry 2Q Chip for Cross Resonance Gates"""
-    runcard = load_runcard(FOLDER)
+    """IQM 5q-chip controlled with a Qblox cluster."""
+    qubits: QubitMap = {i: Qubit.default(i) for i in range(NUM_QUBITS)}
+    #couplers: QubitMap = {f"coupler_{i}": Qubit.coupler(i) for i in (0, 1, 3, 4)}
 
-    # Declare RF Instrument
-    modules = {
-        "qrm_rf0": QrmRf("qrm_rf0", f"{ADDRESS}:18"),  # feedline
-        "qcm_rf0": QcmRf("qcm_rf0", f"{ADDRESS}:4"),  # q0, q1 
-        "qcm_rf1": QcmRf("qcm_rf1", f"{ADDRESS}:6"),  # q2   
-    }
-    controller = QbloxController("qblox_controller", 
-                                 ADDRESS, 
-                                 modules)
+    channels = map_ports(CLUSTER, qubits) # couplers)
+    los = infer_los(CLUSTER)
     
-    # Declare TWPA Inatrument
-    # twpa = SGS100A(name="twpa", address="192.168.0.36")
+    # update channel information beyond connections
+    for i, q in qubits.items():
+        if q.acquisition is not None:
+            channels[q.acquisition] = channels[q.acquisition].model_copy(
+                update={"twpa_pump": None}
+            )
+        if q.probe is not None:
+            channels[q.probe] = channels[q.probe].model_copy(
+                update={"lo": los[i, True]}
+            )
+        if q.drive is not None:
+            channels[q.drive] = channels[q.drive].model_copy(
+                update={"lo": los[i, False]}
+            )
  
-    #  Define the instruments and update all modules with the platform settings
-    instruments = {
-        controller.name: controller,
-        # twpa.name: twpa
-    }
-    instruments.update(modules)
-    instruments = load_instrument_settings(runcard, instruments)
-
-    # #########################################################################################################
- 
-    # Create channel objects
-    channels = ChannelMap()
-    # Readout
-    channels |= Channel(name="feed_in", port=modules["qrm_rf0"].ports("o1"))
-    # Feedback
-    channels |= Channel(name="feed_back", port=modules["qrm_rf0"].ports("i1",out=False))
-
-     # Drive
-    
-    channels |= Channel(name="drive0", port=modules["qcm_rf0"].ports("o1")) # qubit 0
-    channels |= Channel(name="drive1", port=modules["qcm_rf0"].ports("o2")) # qubit 1
-    channels |= Channel(name="drive2", port=modules["qcm_rf1"].ports("o1")) # qubit 2
-    channels |= Channel(name="drive3", port=modules["qcm_rf1"].ports("o2")) # qubit 3
-
-    # create qubit objects
-    qubits, couplers, pairs = load_qubits(runcard)
- 
-    # assign channels to qubits    
-    for q, qubit in qubits.items():
-        qubit.readout = channels["feed_in"]
-        qubit.feedback = channels["feed_back"]
-        if q >= NUM_QUBITS:
-            qubit.drive = channels[f"drive_"]
-        else:
-            qubit.drive = channels[f"drive{q}"]
-            channels[f"drive{q}"].qubit = qubit
-        # qubit.twpa = channels["twpa"]
-    
-    settings = load_settings(runcard)
-
-    # DEBUG: debug folder = report folder ###################################################################
-    import os
-    from datetime import datetime
-
-    debug_folder = f"{ROOT}/debug/{PLATFORM}/"
-    if not os.path.exists(debug_folder):
-        os.makedirs(debug_folder)
-    for name in modules:
-        modules[name]._debug_folder = debug_folder
-    #########################################################################################################
-    return Platform(
-        PLATFORM, qubits, pairs, instruments, settings, resonator_type="2D"
+    controller = Cluster(name=PLATFORM, address=ADDRESS, channels=channels)
+    instruments = {"qblox": controller, "twpa": SGS100A(address="192.168.0.35")}
+    return Platform.load(
+        path=FOLDER, instruments=instruments, qubits=qubits, #couplers=couplers
     )
