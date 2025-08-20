532616bb959833ee7045ed9c05750d07bbbd6588
Alessandro Candido <candido.ale@gmail.com>
2025-08-20 19:37:00 +0200

    build: Add nix files to manage reproducible env

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [inputs.devenv.flakeModule];
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem = {
+        pkgs,
+        config,
+        ...
+      }: {
+        devenv.shells.default = {
+          packages = with pkgs; [
+            poethepoet
+            pre-commit
+            stdenv.cc.cc.lib
+          ];
+
+          env = {
+            LD_LIBRARY_PATH = builtins.concatStringsSep ":" (
+              map (p: "${p}/lib") (
+                with pkgs; [
+                  stdenv.cc.cc.lib
+                  zlib
+                ]
+              )
+            );
+          };
+
+          languages = {
+            python = {
+              enable = true;
+              venv = {
+                enable = true;
+                requirements = ''
+                  qibolab [qblox] @ git+https://github.com/qiboteam/qibolab.git@qblox
+
+                  # dev deps
+                  ipython
+                  pudp
+                '';
+              };
+            };
+          };
+        };
+      };
+    };
+}
