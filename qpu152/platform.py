785b863b742c675e702ad09c4b300c6be55e8c79
jevillegasd <jevillegasd@gmail.com>
2025-11-09 12:13:20 +0000

    update 149, base 152

---
+++ qpu152/platform.py
@@ -0,0 +1,63 @@
+import pathlib
+ 
+from qibolab import Qubit, Hardware, initialize_parameters
+from qibolab._core.instruments.qblox.cluster import Cluster
+from qibolab._core.instruments.qblox.platform import infer_los, map_ports
+from qibolab._core.platform.platform import QubitMap
+from qibolab.instruments.rohde_schwarz import SGS100A
+
+ADDRESS = "192.168.0.20"
+FOLDER  = pathlib.Path(__file__).parent
+PLATFORM = FOLDER.name
+NUM_QUBITS = 5
+ROOT = pathlib.Path.home()
+
+# the only other cluster of the config
+CLUSTER = {
+    "qrm_rf0": (18, {"io1": [0, 1, 2, 3, 4]}),
+    "qcm_rf0": (14, {1: [0], 2: [1]}),
+    "qcm_rf1": (12, {1: [2], 2: [3]}),
+    "qcm_rf2": (10, {1: [4], 2: []}),
+    # "qcm_rf2": (8, {1: [5], 2: []}),
+    "qcm0": (6, {1: [0], 2: [1], 3: [2], 4: [3]}),
+    "qcm1": (4, {1: [4], 2: [], 3: [], 4: []}),
+}
+"""Connections compact representation."""
+
+def create():
+    """TII - QPU 152 controlled with a Qblox cluster."""
+    qubits: QubitMap = {i: Qubit.default(i) for i in range(NUM_QUBITS)}
+    # couplers: QubitMap = {"c1": Qubit(flux="c1/flux", drive="1/drive", probe="0/probe", acquisition="0/acquisition")}
+
+    channels = map_ports(CLUSTER, qubits) # couplers)
+    los = infer_los(CLUSTER)
+
+    # update channel information beyond connections
+    for i, q in qubits.items():
+        if q.acquisition is not None:
+            channels[q.acquisition] = channels[q.acquisition].model_copy(
+                update={"twpa_pump": None}
+            )
+        if q.probe is not None:
+            channels[q.probe] = channels[q.probe].model_copy(
+                update={"lo": los[i, True]}
+            )
+        if q.drive is not None:
+            channels[q.drive] = channels[q.drive].model_copy(
+                update={"lo": los[i, False]}
+            )
+ 
+    controller = Cluster(name=PLATFORM, address=ADDRESS, channels=channels)
+    instruments = {"qblox": controller}# ,"twpa": SGS100A(address="192.168.0.32")} 
+    return Hardware(
+        instruments=instruments, qubits=qubits, #couplers=couplers
+    )
+
+
+if __name__ == "__main__":
+    # You may need to rename the file to something different than platform.py to run this script.
+    platform = create()
+    parms = initialize_parameters(platform)
+    (FOLDER / "parameters.json").write_text(
+        parms.model_dump_json(indent=4)
+    )
