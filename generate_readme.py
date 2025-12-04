8c6dec28d6d89b61a4fef3348e40bde3819102c9
Roy Stegeman <roystegeman@live.nl>
2025-12-04 13:57:30 +0400

    handle if the T1 or T2 unc is None

--- generate_readme.py
+++ generate_readme.py
@@ -95,8 +95,14 @@ def create_fidelity_table(fidelity: dict) -> str:
     for qubit, qinfo in fidelity.items():
         assingment_fidelity_str = f"{qinfo['readout_fidelity']/2 + 0.5:.2f}"
         # t1 and t2 are in ns, convert to µs
-        t1_str = f"{qinfo['t1'][0]/1e3:.1f} ± {qinfo['t1'][1]/1e3:.1f}"
-        t2_str = f"{qinfo['t2'][0]/1e3:.1f} ± {qinfo['t2'][1]/1e3:.1f}"
+        if qinfo['t1'][1] is None:
+            t1_str = f"{qinfo['t1'][0]/1e3:.1f}"
+        else:
+            t1_str = f"{qinfo['t1'][0]/1e3:.1f} ± {qinfo['t1'][1]/1e3:.1f}"
+        if qinfo['t2'][1] is None:
+            t2_str = f"{qinfo['t2'][0]/1e3:.1f}"
+        else:
+            t2_str = f"{qinfo['t2'][0]/1e3:.1f} ± {qinfo['t2'][1]/1e3:.1f}"
         # NOTE: these are all 0.0, null in the calibration.json files available now,
         # so I'm not too sure about the formatting
         gate_infidelity_cv = -qinfo["rb_fidelity"][0]
