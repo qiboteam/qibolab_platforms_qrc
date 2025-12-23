eda0c1d081da78db7ba4e23353be4f1751b33d26
RoyStegeman <roystegeman@live.nl>
2025-12-23 08:50:35 +0000

    add qpu149 platform

---
+++ qpu149/README.md
@@ -0,0 +1,34 @@
+# qw5q_platinum
+
+## Native Gates
+**Single Qubit**: MZ, RX, RX12
+
+**Two Qubit**: CZ, iSWAP
+
+## Topology
+**Number of qubits**: 5
+
+**Qubits**: 0, 1, 2, 3, 4
+
+```mermaid
+---
+config:
+layout: elk
+---
+graph TD;
+    0((0)) <--> 2((2));
+    1((1)) <--> 2((2));
+    2((2)) <--> 3((3));
+    2((2)) <--> 4((4));
+```
+
+
+## Qubit fidelity and coherence times
+
+| Qubit | Assignment Fidelity | T1 (µs) | T2 (µs) | Gate infidelity (e-3) |
+| --- | --- | --- | --- | --- |
+| 0 | 0.96 | 47.3 ± 2.5 | 11.1 ± 0.4 | 0.0 |
+| 1 | 0.97 | 37.1 ± 1.2 | 23.2 ± 0.7 | 0.0 |
+| 2 | 0.98 | 31.9 ± 0.9 | 9.0 ± 0.2 | 0.0 |
+| 3 | 0.95 | 30.8 ± 0.8 | 26.1 ± 1.1 | 0.0 |
+| 4 | 0.99 | 33.7 ± 0.9 | 3.4 ± 0.1 | 0.0 |
---
+++ qpu149/platform.py
@@ -0,0 +1,53 @@
+import pathlib
+
+from qibolab import Hardware, Qubit
+from qibolab._core.instruments.qblox.cluster import Cluster
+from qibolab._core.instruments.qblox.platform import infer_los, map_ports
+from qibolab._core.platform.platform import QubitMap
+from qibolab.instruments.rohde_schwarz import SGS100A
+
+FOLDER = pathlib.Path(__file__).parent
+NAME = "qpu149"
+ADDRESS = "192.168.0.2"
+
+CLUSTER = {
+    "qcm_rf2": (14, {1: [0], 2: [1]}),
+    "qcm_rf1": (12, {1: [2], 2: [3]}),
+    "qcm_rf0": (10, {2: [4]}),
+    "qrm_rf": (20, {"io1": [0, 1, 2, 3, 4]}),
+}
+"""Connections compact representation."""
+
+def create():
+    """foundry QPU149 controlled with a Qblox cluster."""
+    qubits: QubitMap = {i: Qubit.default(i) for i in range(5)}
+
+    # Create channels and connect to instrument ports
+    channels = map_ports(CLUSTER, qubits)
+    los = infer_los(CLUSTER)
+
+    # update channel information beyond connections
+    for i, q in qubits.items():
+        if q.acquisition is not None:
+            channels[q.acquisition] = channels[q.acquisition].model_copy(
+                update={"twpa_pump": "twpa"}
+            )
+        if q.probe is not None:
+            channels[q.probe] = channels[q.probe].model_copy(
+                update={"lo": los[i, True]}
+            )
+        if q.drive is not None:
+            channels[q.drive] = channels[q.drive].model_copy(
+                update={"lo": los[i, False]}
+            )
+
+    controller = Cluster(name=NAME, address=ADDRESS, channels=channels)
+    instruments = {
+        "qblox": controller,
+        "twpa": SGS100A(address="192.168.0.37", turn_off_on_disconnect=False),
+    }
+    return Hardware(
+        path=FOLDER,
+        instruments=instruments,
+        qubits=qubits,
+    )
