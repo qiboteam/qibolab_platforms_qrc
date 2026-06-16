db5c69bcff0e4d079fa96f4a98fde7fec3b1d347
jevillegasd <jevillegasd@gmail.com>
2026-06-16 09:32:24 +0400

    add queues and update calibration qpu 175 and 176

---
+++ qpu175/platform.py
@@ -0,0 +1,54 @@
+import pathlib
+
+from qibolab import Platform, Qubit
+from qibolab._core.instruments.qblox.cluster import Cluster
+from qibolab._core.instruments.qblox.platform import infer_los, infer_mixers, map_ports
+from qibolab._core.platform.platform import QubitMap
+from qibolab.instruments.rohde_schwarz import SGS100A
+
+FOLDER = pathlib.Path(__file__).parent
+NAME = "qpu175"
+ADDRESS = "192.168.0.20"
+
+CLUSTER = {
+    "qrm_rf": (19, {"io1": [0, 1, 2, 3]}),
+    "qcm_rf0": (14, {1: [0], 2: [2]}),
+    "qcm_rf1": (8, {1: [1], 2: [3]})
+}
+"""Connections compact representation."""
+
+
+def create():
+    """qpu 175 chip controlled with a Qblox cluster."""
+    qubits: QubitMap = {i: Qubit.default(i) for i in range(4)}
+
+    # Create channels and connect to instrument ports
+    channels = map_ports(CLUSTER, qubits)
+    los = infer_los(CLUSTER)
+    mixers = infer_mixers(CLUSTER)
+
+    # update channel information beyond connections
+    for i, q in qubits.items():
+        if q.acquisition is not None:
+            channels[q.acquisition] = channels[q.acquisition].model_copy(
+                update={"twpa_pump": "twpa"}
+            )
+        if q.probe is not None:
+            channels[q.probe] = channels[q.probe].model_copy(
+                update={"lo": los[i, True], "mixer": mixers[i, True]}
+            )
+        if q.drive is not None:
+            channels[q.drive] = channels[q.drive].model_copy(
+                update={"lo": los[i, False], "mixer": mixers[i, False]}
+            )
+
+    controller = Cluster(name=NAME, address=ADDRESS, channels=channels)
+    instruments = {
+        "qblox": controller,
+        "twpa": SGS100A(address="192.168.0.32", turn_off_on_disconnect=False),
+    }
+    return Platform.load(
+        path=FOLDER,
+        instruments=instruments,
+        qubits=qubits,
+    )
---
+++ qpu176/README.md
@@ -0,0 +1,28 @@
+# qpu176
+
+## Native Gates
+**Single Qubit**: MZ, RX, RX12
+
+**Two Qubit**: CR
+
+## Topology
+**Number of qubits**: 2
+
+**Qubits**: 0, 1
+
+```mermaid
+---
+config:
+layout: elk
+---
+graph TD;
+    0((0)) <--> 1((1));
+```
+
+
+## Qubit fidelity and coherence times
+
+| Qubit | Assignment Fidelity | T1 (µs) | T2 (µs) | Gate infidelity (e-3) |
+| --- | --- | --- | --- | --- |
+| 0 | 0.0 | 0.0 ± 0.0 | 0.0 ± 0.0 | 0.0 |
+| 1 | 0.0 | 0.0 ± 0.0 | 0.0 ± 0.0 | 0.0 |
