3f761ae80cc86a71301e728a81f2be484c358ce5
Alessandro Candido <candido.ale@gmail.com>
2026-07-09 18:37:16 +0400

    docs: Update platform fidelity table

--- generate_readme.py
+++ generate_readme.py
@@ -103,16 +103,16 @@ def create_fidelity_table(fidelity: dict) -> str:
 | --- | --- | --- | --- | --- |
 """
     for qubit, qinfo in fidelity.items():
-        assingment_fidelity_str = f"{qinfo['readout_fidelity']/2 + 0.5:.2f}"
+        assingment_fidelity_str = f"{qinfo['readout_fidelity'] / 2 + 0.5:.2f}"
         # t1 and t2 are in ns, convert to µs
         if qinfo["t1"][1] is None:
-            t1_str = f"{qinfo['t1'][0]/1e3:.1f}"
+            t1_str = f"{qinfo['t1'][0] / 1e3:.1f}"
         else:
-            t1_str = f"{qinfo['t1'][0]/1e3:.1f} ± {qinfo['t1'][1]/1e3:.1f}"
+            t1_str = f"{qinfo['t1'][0] / 1e3:.1f} ± {qinfo['t1'][1] / 1e3:.1f}"
         if qinfo["t2"][1] is None:
-            t2_str = f"{qinfo['t2'][0]/1e3:.1f}"
+            t2_str = f"{qinfo['t2'][0] / 1e3:.1f}"
         else:
-            t2_str = f"{qinfo['t2'][0]/1e3:.1f} ± {qinfo['t2'][1]/1e3:.1f}"
+            t2_str = f"{qinfo['t2'][0] / 1e3:.1f} ± {qinfo['t2'][1] / 1e3:.1f}"
         # NOTE: these are all 0.0, null in the calibration.json files available now,
         # so I'm not too sure about the formatting
         gate_infidelity_cv = 1 - qinfo["rb_fidelity"][0]
@@ -121,7 +121,7 @@ def create_fidelity_table(fidelity: dict) -> str:
             gate_infidelity_str = "0.0"
         elif gate_infidelity_cv is not None and gate_infidelity_err is not None:
             gate_infidelity_str = (
-                f"{gate_infidelity_cv:.1f} ± {gate_infidelity_err:.1f}"
+                f"{gate_infidelity_cv:.1e} ± {gate_infidelity_err:.1e}"
             )
         else:
             gate_infidelity_str = "N/A"
@@ -158,7 +158,6 @@ def create_readme(info: dict) -> str:
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(
         prog="generate_readme",
         description="Automatically generate the README.md for the given input platform/s.",
